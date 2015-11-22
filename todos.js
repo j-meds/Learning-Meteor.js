1c821668f222c1540d27cd07fcc7bb1372178974
josemedina760 <josemedina760@gmail.com>
2015-11-21 18:50:29 -0800

    To do app Completed with Full crud

--- todos.js
+++ todos.js
@@ -1,20 +1,53 @@
+Tasks = new Mongo.Collection('tasks')
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function(){
+      return Tasks.find({}, {sort : {createdAt: -1}});
+    }
   });
-}
 
-Tasks = new Mongo.Collection('tasks');
+  Template.body.events({
+    'submit .new-task': function(event){
+      //prevent default browser form submit
+      event.preventDefault();
+
+      console.log(event);
+      console.log(event.target.task.value);
+
+      //Get value form element
+      var text = event.target.task.value;
+
+      //object being saved
+      if(text){
+      var item = {text:text, createdAt: new Date()};
+      }
+
+      //insert/saving a task into a collection
+      Tasks.insert(item);
+
+      event.target.task.value = '';
+
+    }
+  });
+
+  Template.body.events({
+    'click .toggle-checked': function(){
+      //set the checked property of the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    'click .delete': function(){
+      Tasks.remove(this._id);
+    }
+  });
+
+};
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-      tasks: function() {
-        return Tasks.find({});
-      }
+    // code to run on server at startup
   });
 }
